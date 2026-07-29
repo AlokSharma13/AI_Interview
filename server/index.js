732ab87b042fdcf3242a28f2408d79fd06a44d26
Alok Sharma <2k23.csai2313910@gmail.com>
2026-07-29 15:39:33 +0530

    Add root endpoint to send 'start' response

--- server/index.js
+++ server/index.js
@@ -17,7 +17,9 @@ app.use(cors({
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.get("/",(req,res)=>{
+    res.send("start");
+});
 app.use("/api/auth" , authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/interview" , interviewRouter)
