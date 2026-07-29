2ceaf734ef1dc4cf6b0ef4a01d9ff703dfc7985e
Alok Sharma <2k23.csai2313910@gmail.com>
2026-07-29 22:26:38 +0530

    Remove console log from user retrieval error handling
    Removed console log for error handling in user retrieval.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -10,7 +10,6 @@ export const getCurrentUser = async (req,res) => {
         }
         return res.status(200).json(user)
     } catch (error) {
-        console.log(error);
         return res.status(500).json({message:`failed to get currentUser ${error}`})
     }
 }
